2b20e97c8291fa2c1ad6f7f82e26d76a37f4862c
finnsetchell <finn@setchell.org>
2026-07-06 16:43:07 +0200

    Initial project setup

---
+++ src/main/java/dev/finndog/configsearch/ConfigSearchClient.java
@@ -0,0 +1,9 @@
+package dev.finndog.configsearch;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ConfigSearchClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+	}
+}
